b726e41d65b25fbd7e8a66b43303ca69751c97b7
Beni <jbenipayo@rayasolutionsph.com>
2023-09-14 00:18:14 +0800

    kirk test

--- bairesort_app/api.py
+++ bairesort_app/api.py
@@ -3,8 +3,4 @@ import frappe
 @frappe.whitelist()
 def call_my_custom_function(docname):
 
-<<<<<<< HEAD
     return "KIRK TEST"
-=======
-    return "test"
->>>>>>> 08a9a7ceb19ffc183b9112f1ac34b81ca0497ae8
