26b161081c21cbf75297b2187fc9829eb990de69
Beni <jbenipayo@rayasolutionsph.com>
2023-09-14 11:20:42 +0800

    add

--- bairesort_app/api.py
+++ bairesort_app/api.py
@@ -3,4 +3,12 @@ import frappe
 @frappe.whitelist()
 def call_my_custom_function(docname):
 
+<<<<<<< HEAD
     return "KIRK TEST"
+=======
+<<<<<<< HEAD
+    return "test"
+=======
+    return "Please I want you to work"
+>>>>>>> 1a17530 (test1)
+>>>>>>> 21d244d (idk whats happening)
