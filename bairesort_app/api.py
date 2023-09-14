e82f047bc0315e8afca0b81a1633c2e197bcd347
Beni <jbenipayo@rayasolutionsph.com>
2023-09-14 11:37:19 +0800

    kirk

--- bairesort_app/api.py
+++ bairesort_app/api.py
@@ -3,20 +3,4 @@ import frappe
 @frappe.whitelist()
 def call_my_custom_function(docname):
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    return "KIRK TEST"
-=======
-<<<<<<< HEAD
-    return "test"
-=======
-    return "Please I want you to work"
->>>>>>> 1a17530 (test1)
->>>>>>> 21d244d (idk whats happening)
-=======
-    return "api.py + client script test = Successful(KIRK)"
->>>>>>> 198bceb (please work)
-=======
-    return "kirk test"
->>>>>>> 300abe8 (changes)
+    return "kirk"
